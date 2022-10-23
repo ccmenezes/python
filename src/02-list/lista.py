467bdce6f6b3c74ebc31170fc444a3935ece7e00
Claudia Menezes <claudia@claudia-asus.Home>
2022-10-23 19:10:46 +0100

    rename folder

---
+++ src/02-list/lista.py
@@ -0,0 +1,21 @@
+## Class 02 
+## Lists
+
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+
+# Add fruits
+fruits.append("limon")
+print(fruits)
+
+# Count the fruits
+print(len(fruits))
+
+# Remove a fruit
+fruits.remove("orange")
+print(fruits)
+
+# Reverse the fruit list
+print(fruits)
+fruits.reverse()
+print(fruits)
